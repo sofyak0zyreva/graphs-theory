c20bb801bda7176dd2c5e030b583c9dbf8893339
sofyak0zyreva <k81sofia@gmail.com>
2026-04-08 18:18:43 +0300

    feat: pr results

--- scripts/lagraph/lagraph_pr.c
+++ scripts/lagraph/lagraph_pr.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <suitesparse/LAGraph.h>
 #include <suitesparse/GraphBLAS.h>
+#include "utils.h"
 
 int main(int argc, char **argv)
 {
@@ -16,27 +17,9 @@ int main(int argc, char **argv)
 
 	int n_iters = atoi(argv[2]);
 
-	GrB_init(GrB_BLOCKING);
-	LAGraph_Init(msg);
+	LAGraph_Graph G = createGraph(mtx_path);
 
-	// load matrix
-	GrB_Matrix A = NULL;
-	FILE *f = fopen(mtx_path, "r");
-	if (f == NULL)
-	{
-		printf("Failed to open file: %s\n", mtx_path);
-		return 1;
-	}
-
-	LAGraph_MMRead(&A, f, NULL);
-
-	fclose(f);
-
-	// create graph
-	LAGraph_Graph G = NULL;
-	LAGraph_New(&G, &A, LAGraph_ADJACENCY_DIRECTED, NULL);
-
-	// required
+	// required for better performance
 	LAGraph_Cached_OutDegree(G, msg);
 	LAGraph_Cached_AT(G, msg);
 
@@ -44,16 +27,21 @@ int main(int argc, char **argv)
 	GrB_Vector centrality;
 	int iters;
 	float damping = 0.85;
-	float tol = 1e-4;
+	float tol = 1e-6;
 	int itermax = 100;
 
 	for (int i = 0; i < n_iters; i++)
 	{
 		double t0 = LAGraph_WallClockTime();
 
-		LAGr_PageRank(&centrality, &iters, G, damping, tol, itermax, msg);
+		int status = LAGr_PageRank(&centrality, &iters, G, damping, tol, itermax, msg);
 
 		double t1 = LAGraph_WallClockTime();
+
+		if (status != GrB_SUCCESS)
+		{
+			printf("PAGERANK failed\n");
+		}
 
 		double elapsed_ms = (t1 - t0) * 1000.0;
 
@@ -63,10 +51,9 @@ int main(int argc, char **argv)
 		GrB_free(&centrality);
 	}
 
-	LAGraph_Delete(&G, NULL);
-	GrB_free(&A);
+	LAGraph_Delete(&G, msg);
 
-	LAGraph_Finalize(NULL);
+	LAGraph_Finalize(msg);
 	GrB_finalize();
 
 	return 0;
