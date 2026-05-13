0ddccf871527238035e86efc0e859683256bea85
sofyak0zyreva <k81sofia@gmail.com>
2026-05-13 10:27:05 +0300

    feat: save plots

--- scripts/show_results.py
+++ scripts/show_results.py
@@ -2,42 +2,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from scripts.vars import *
 
-# Загрузка данных
-df = pd.read_csv('/Users/sofyakozyreva/graphs-theory/scripts/benchmark/results/bfs_results.csv')
 
-# Настройка стиля
-sns.set_theme(style="whitegrid")
-plt.figure(figsize=(14, 8))
+def show(path, output_path, name):
+    df = pd.read_csv(path)
 
-# Построение графика
-# Сделаем 'nodes' категориальным признаком для корректного отображения групп
-df['nodes'] = df['nodes'].astype(str)
+    sns.set_theme(style="whitegrid")
+    plt.figure(figsize=(14, 8))
 
-ax = sns.barplot(
-    data=df,
-    x='graph',
-    y='mean',
-    hue='nodes',
-    palette='viridis'
-)
+    df['nodes'] = df['nodes'].astype(str)
 
-# Установка логарифмической шкалы
-ax.set_yscale("log")
+    ax = sns.barplot(
+        data=df,
+        x='graph',
+        y='mean',
+        hue='nodes',
+        palette='viridis'
+    )
 
-# Добавление подписей mean над столбиками
-for container in ax.containers:
-    # Для логарифмической шкалы нужно аккуратно располагать текст,
-    # чтобы он не наезжал на границы
-    labels = [f'{val:.2f}' if val > 0 else '' for val in container.datavalues]
-    ax.bar_label(container, labels=labels, padding=3, fontsize=9, rotation=45)
+    ax.set_yscale("log")
 
-# Настройка осей и легенды
-plt.title('BFS Performance by Graph and Node Count', fontsize=16)
-plt.xlabel('Graph Name', fontsize=12)
-plt.ylabel('Mean Time (log scale)', fontsize=12)
-plt.xticks(rotation=15, ha='right')
-plt.legend(title='Nodes', loc='upper left')
+    for container in ax.containers:
+        labels = [f'{val:.2f}' if val >
+                  0 else '' for val in container.datavalues]
+        ax.bar_label(container, labels=labels,
+                     padding=3, fontsize=9, rotation=45)
 
-plt.tight_layout()
-plt.show()
+    # plt.title('BFS Performance by Graph and Node Count', fontsize=16)
+    plt.title(name, fontsize=16)
+    plt.xlabel('Graph Name', fontsize=12)
+    plt.ylabel('Mean Time (log scale)', fontsize=12)
+    plt.xticks(rotation=15, ha='right')
+    plt.legend(title='Nodes', loc='upper left')
+
+    plt.tight_layout()
+    plt.savefig(output_path, dpi=300, bbox_inches="tight")
+    plt.show()
+
+
+if __name__ == "__main__":
+    path = OUTPUT_CSV
+    show(path / "bfs_results.csv", path / "bfs.png", "BFS")
+    show(path / "tc_results.csv", path / "tc.png", "TC")
+    show(path / "pr_results.csv", path / "pr.png", "PR")
--- scripts/vars.py
+++ scripts/vars.py
@@ -2,10 +2,21 @@ import os
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[2]
+print(ROOT)
 
 DATA_DIR = Path("data")
 
 OUT_DIR = Path("data/gr")
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
+THREADS = 1
+RUNS = 30
 
+NS = [1, 2, 4, 8]
+
+REPO_ROOT = ROOT / "graphs-theory"
+
+OUTPUT_CSV = REPO_ROOT / "scripts/benchmark/results/"
+GALOIS_BIN = "./build/lonestar/analytics/distributed/"
+GALOIS_ROOT = ROOT / "Galois"
+print(OUTPUT_CSV)
