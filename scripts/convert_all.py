8390d94d6a286316089fd521dd1d1dbbaa070a5f
sofyak0zyreva <k81sofia@gmail.com>
2026-05-13 10:28:18 +0300

    feat: tune graph conversion for diff algos

--- scripts/convert_all.py
+++ scripts/convert_all.py
@@ -1,17 +1,82 @@
 import subprocess
 from pathlib import Path
-from scripts.datasets import collect_all_datasets
+from scripts.datasets import *
 from scripts.vars import *
 
-GRAPH_CONVERT = "/Users/sofyakozyreva//Galois/build/tools/graph-convert/graph-convert"
+GRAPH_CONVERT = GALOIS_ROOT / "build/tools/graph-convert/graph-convert"
 
 
 def run(cmd):
     # print(" ".join(cmd))
     subprocess.run(cmd, check=True)
 
+def convert_mtx_to_gr_weighted(mtx_file: Path):
+    name = mtx_file.stem
 
-def convert_mtx_to_gr(mtx_file: Path):
+    gr_file = OUT_DIR / f"{name}.gr"
+    tgr_file = OUT_DIR / f"{name}.tgr"
+
+    # mtx -> gr
+    run([
+        GRAPH_CONVERT,
+        "--edgelist2gr",
+        str(mtx_file),
+        str(gr_file),
+        "--edgeType=float32"
+    ])
+
+    # gr -> gr + reversed edges
+    run([
+        GRAPH_CONVERT,
+        "--gr2sgr",
+        str(gr_file),
+        str(gr_file),
+        "--edgeType=float32"
+    ])
+
+    # gr -> tgr
+    run([
+        GRAPH_CONVERT,
+        "--gr2tgr",
+        str(gr_file),
+        str(tgr_file)
+    ])
+
+    print(f"Done: {name}")
+
+def convert_mtx_to_gr_undir(mtx_file: Path):
+    name = mtx_file.stem
+
+    gr_file = OUT_DIR / f"{name}.gr"
+    tgr_file = OUT_DIR / f"{name}.tgr"
+
+    # mtx -> gr
+    run([
+        GRAPH_CONVERT,
+        "--edgelist2gr",
+        str(mtx_file),
+        str(gr_file)
+    ])
+
+    # gr -> gr + reversed edges
+    run([
+        GRAPH_CONVERT,
+        "--gr2sgr",
+        str(gr_file),
+        str(gr_file)
+    ])
+
+    # gr -> tgr
+    run([
+        GRAPH_CONVERT,
+        "--gr2tgr",
+        str(gr_file),
+        str(tgr_file)
+    ])
+
+    print(f"Done: {name}")
+
+def convert_mtx_to_gr_dir(mtx_file: Path):
     name = mtx_file.stem
 
     gr_file = OUT_DIR / f"{name}.gr"
@@ -43,9 +108,16 @@ def main(name):
     # print(f"Found {len(mtx_files)} graphs")
 
     # for f in mtx_files:
-    convert_mtx_to_gr(mtx_files)
+    # convert_mtx_to_gr(mtx_files)
 
 
 if __name__ == "__main__":
-    for group, name in collect_all_datasets():
-        main(name)
+    # for group, name in list(set(BFS_DATASETS + TC_DATASETS + SSSP_DATASETS)):
+    #     mtx_files = DATA_DIR / name / (name + ".mtx")
+    #     convert_mtx_to_gr_undir(mtx_files)
+    # for group, name in list(set(PAGERANK_DATASETS)):
+    #     mtx_files = DATA_DIR / name / (name + ".mtx")
+    #     convert_mtx_to_gr_dir(mtx_files)
+    for group, name in list(set(SSSP_DATASETS)):
+        mtx_files = DATA_DIR / name / (name + ".mtx")
+        convert_mtx_to_gr_weighted(mtx_files)
